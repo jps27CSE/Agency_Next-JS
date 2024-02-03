85c047410860d312a1a60d205c489b43bf74e9c5
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2024-02-04 01:00:28 +0600

    single blog post page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./singlePost.module.css";
 
 const SinglePostPage = () => {
-  return <div>SinglePostPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/post.png" alt="" fill className={styles.img} />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image
+            className={styles.avatar}
+            width={50}
+            height={50}
+            src="/post.png"
+            alt=""
+          />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Terry </span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01.01.2024 </span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint nisi
+          cupiditate soluta itaque debitis! Suscipit ipsum atque temporibus
+          quibusdam! Tempora quos blanditiis minima consequatur alias optio
+          ipsam aliquam nesciunt vero.
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePostPage;
