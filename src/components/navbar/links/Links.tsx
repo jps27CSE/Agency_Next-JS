6d5a04e6029173a08b9ae25bea8acc453588a572
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2024-02-03 20:22:00 +0600

    Responsive Navbar

--- src/components/navbar/links/Links.tsx
+++ src/components/navbar/links/Links.tsx
@@ -1,7 +1,12 @@
+"use client";
+import { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/NavLink";
+import Image from "next/image";
 
 const Links = () => {
+  const [open, setOpen] = useState(false);
+
   const links = [
     { title: "Homepage", path: "/" },
     {
@@ -22,18 +27,35 @@ const Links = () => {
   const isAdmin = true;
 
   return (
-    <div className={styles.links}>
-      {links.map((link) => (
-        <NavLink item={link} key={link.title} />
-      ))}
-      {session ? (
-        <>
-          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
-          <button className={styles.logout}>Logout</button>
-        </>
-      ) : (
-        <NavLink item={{ title: "Login", path: "/login" }} />
-      )}
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )}
+        <Image
+          className={styles.menuButton}
+          src="/menu.png"
+          alt=""
+          width={30}
+          height={30}
+          onClick={() => setOpen((prev) => !prev)}
+        />
+        {open && (
+          <div className={styles.mobileLinks}>
+            {links.map((link) => (
+              <NavLink item={link} key={link.title} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
